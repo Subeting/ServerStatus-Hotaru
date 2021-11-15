e77682dbd91e99c89fd632992eb856c60f7a1cfa
Subeting <68261543+Subeting@users.noreply.github.com>
2021-11-15 19:24:34 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+rm -rf /etc/network/if-pre-up.d/iptables
 
 #=================================================
 #	System Required: CentOS/Debian/Ubuntu
